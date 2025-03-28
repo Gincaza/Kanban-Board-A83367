310bb66d1a996984060eea8a26745a0f88f06df4
moth <dinocruz434@gmail.com>
2025-03-28 19:03:00 +0000

    Tags armazenadas no local_storage pra persistencia

--- item.py
+++ item.py
@@ -85,6 +85,24 @@ class Item(ft.Container):
                 ft.Text(label, bgcolor=ft.Colors.LIGHT_BLUE)
                 for label in self.labels
             ]
+
+            # Atualizar o client_storage
+            stored_users, current_user_data = self.list.board.app.get_current_user_data()
+            if current_user_data:
+                boards = current_user_data.get("boards") or {}
+                board_data = boards.get(str(self.list.board.board_id)) or {}
+                lists = board_data.get("lists") or {}
+                list_data = lists.get(str(self.list.board_list_id)) or {}
+                items = list_data.get("items", {})
+                items[str(self.item_id)]["labels"] = self.labels  # Atualizar as tags
+                list_data["items"] = items
+                lists[str(self.list.board_list_id)] = list_data
+                board_data["lists"] = lists
+                boards[str(self.list.board.board_id)] = board_data
+                current_user_data["boards"] = boards
+                stored_users[self.page.client_storage.get("current_user")] = current_user_data
+                self.page.client_storage.set("users", stored_users)
+
             self.page.close(dialog)
             self.page.update()
 
